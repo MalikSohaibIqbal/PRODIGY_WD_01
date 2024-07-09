cca15bdfd55b53642dcf4db10514e5947c9ac82d
Malik Sohaib Iqbal <163728585+MalikSohaibIqbal@users.noreply.github.com>
2024-07-10 00:40:57 +0500

    body

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
